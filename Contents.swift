79cf8b33c59a7d5e72e8a32c6828a3f7cace1c10
Gonzalo Campos <erick.campos@wizeline.com>
2020-01-03 21:29:47 -0600

    Swift flow of control

--- Contents.swift
+++ Contents.swift
@@ -78,3 +78,59 @@ let n: Int = 3
 // let i = m + n ----> This code won't compile!! :P
 let i = m + Double(n) // This one will!!!
 
+print("")
+// Lesson 1.4
+
+let temperature2 = 104
+if temperature2 >= 100 {
+    print("The temperature is > 100: \(temperature2)")
+} else {
+    print("It's cold outside, brr :v")
+}
+
+let temperature = 70
+
+if temperature >= 65 && temperature <= 75 {
+  print("The temperature is just right.")
+} else if temperature < 65 {
+  print("It is too cold.")
+} else {
+  print("It is too hot.")
+}
+
+var isPluggedIn = false
+var hasBatteryPower = false
+
+if isPluggedIn || hasBatteryPower {
+    print("You can use your laptop")
+} else {
+    print("😱")
+}
+
+let numberOfWheels = 2
+
+switch numberOfWheels {
+case 1:
+    print("Unicycle")
+case 2:
+    print("Bicycle")
+case 3:
+    print("Tricycle")
+case 4:
+    print("Quadcycle")
+default:
+    print("That's a lot of wheels")
+}
+
+for i in 0...10 {
+    print("The actual number is: \(i)")
+}
+
+print("")
+
+// Ternary operator
+let ab = 15
+let b = 10
+let largest = ab > b ? ab : b
+
+print(largest)
